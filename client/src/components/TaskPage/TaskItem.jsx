e651afd85345f3e2ce6f8310f81dcbe574549013
Facundo Barberia <facundobarberia@gmail.com>
2024-12-30 01:43:25 -0300

    TaskItem

--- client/src/components/TaskPage/TaskItem.jsx
+++ client/src/components/TaskPage/TaskItem.jsx
@@ -4,11 +4,28 @@ const TaskItem = ({ task }) => {
 
 
   return (
-    <li style={{ listStyleType: 'none' }}>
+    <div className=' rounded-xl bg-white p-5 text-black pt-serif-bold m-5 flex justify-between'>
       <div>
         {task.title}
+        <p className='pt-serif-regular'>{task.description}</p>
       </div>
-    </li>
+      <div>
+        {task.completed ? null : <button>completar</button>}
+        <div className='flex flex-col gap-2 items-end'>
+          <button className='bg-gradient-to-r from-purple-400 to-red-400 text-white pt-serif-bold  px-3 py-1 '>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" width="18px" height="18px" viewBox="-3.5 0 19 19" className="cf-icon-svg">
+            <path d="M11.383 13.644A1.03 1.03 0 0 1 9.928 15.1L6 11.172 2.072 15.1a1.03 1.03 0 1 1-1.455-1.456l3.928-3.928L.617 5.79a1.03 1.03 0 1 1 1.455-1.456L6 8.261l3.928-3.928a1.03 1.03 0 0 1 1.455 1.456L7.455 9.716z"/>
+            </svg>
+          </button>
+          <button className='bg-gradient-to-r from-orange-400 to-yellow-400 text-white pt-serif-bold  px-3 py-1 '>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 16 16" fill="none">
+                <path d="M8.29289 3.70711L1 11V15H5L12.2929 7.70711L8.29289 3.70711Z" fill="#000000"/>
+                <path d="M9.70711 2.29289L13.7071 6.29289L15.1716 4.82843C15.702 4.29799 16 3.57857 16 2.82843C16 1.26633 14.7337 0 13.1716 0C12.4214 0 11.702 0.297995 11.1716 0.828428L9.70711 2.29289Z" fill="#000000"/>
+            </svg>
+        </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -13,9 +13,9 @@ const TaskPage = () => {
 
 
   useEffect(() => {
-    console.log('User:', user);
+    const token = localStorage.getItem('token');
     if (user) {
-      fetchTasks(user.token);
+      fetchTasks(token);
     }
   }, [user]);
 
@@ -89,18 +89,37 @@ const TaskPage = () => {
   return (
     <div className="flex ">
       <div className=' flex-col flex justify-between max-h-[100%] bg-[#141414] h-[100vh]'>
+        <div>
         <TypingEffect />
+        <div className='bg-white text-black pt-serif-bold p-3 mt-6 flex justify-between'>
+          <div>Agregar tarea.</div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+              <path d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15" stroke="#1C274C" strokeWidth="1.5" strokeLinecap="round"/>
+              <path d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
+          </div>
+        </div>
         <div className=' self-center '>
           <button 
             onClick={handleLogout}
-            className=" mb-5 p-3 border-0 bg-gradient-to-r from-purple-300 to-red-300 text-white rounded-lg hover:from-purple-400 hover:to-red-400 transition duration-300 ease-in-out transform hover:scale-105 pt-serif-bold"
+            className=" mb-5 p-3 border-0 bg-gradient-to-r from-blue-300 to-violet-300 text-white rounded-lg hover:from-purple-400 hover:to-red-400 transition duration-300 ease-in-out transform hover:scale-105 pt-serif-bold"
           >
-            Desloguearse
+            Desloguearme
           </button>
         </div>
       </div>
-      <div className='flex flex-col bg-[#141414] m-8 rounded-xl w-[100%]'>
-        <div>
+      <div className='flex  bg-[#141414] mx-8 my-5 rounded-xl w-[100%]'>
+        <div className=' bg-black m-5 rounded-lg w-[50%] h-[95%]'>
+          <div className='bg-gradient-to-r from-purple-400 to-red-400 text-white rounded-t-lg text-center pt-serif-bold text-2xl py-2'> Pendientes</div>
+          <TaskList
+            tasks={tasks}
+            onToggleCompletion={handleUpdateTask}
+            onEditTask={handleUpdateTask}
+            onDeleteTask={handleDeleteTask}
+            />
+        </div>
+        <div className=' bg-black m-5 rounded-lg w-[50%] h-[95%]'>
+          <div className='bg-gradient-to-r from-blue-400 to-green-400 text-white rounded-t-lg text-center pt-serif-bold text-2xl py-2'>Completadas</div>
           <TaskList
             tasks={tasks}
             onToggleCompletion={handleUpdateTask}
